a1772391dd68da43fc23d9a0f6f06c7d41b3a728
Tej <tejpartapsingh91@gmail.com>
2020-04-24 16:15:12 -0400

    revise

--- cdnjs/src/App.js
+++ cdnjs/src/App.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import Library from "./components/Library";
 import ErrorMessage from "./components/ErrorMessage";
