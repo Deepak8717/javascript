50ea3efccd8bfb83a37331d9a45ae3f31748fb4a
Tej <tejpartapsingh91@gmail.com>
2020-04-24 16:19:41 -0400

    revise

--- cdnjs/src/components/Library.js
+++ cdnjs/src/components/Library.js
@@ -3,7 +3,7 @@ import CurrentLib from "./CurrentLib";
 import Libraries from "./Libraries";
 
 const Library = ({ data, setData }) => {
-  const { results, currentLib } = data.content;
+  const { results } = data.content;
   const cssFiles = results
     .filter((i) => i.latest.includes(`.css`))
     .sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
@@ -15,12 +15,7 @@ const Library = ({ data, setData }) => {
       <div className="container mb-3">
         <div className="row">
           <div className="col col-12">
-            <CurrentLib
-              data={data}
-              className={`currentLib ${
-                currentLib && currentLib.length !== 0 ? "foo" : "boo"
-              }`}
-            />
+            <CurrentLib data={data} />
             <h1 className="my-3">CSS</h1>
             <Libraries set={cssFiles} data={data} setData={setData} />
             <h1 className="my-3">JavaScript</h1>
