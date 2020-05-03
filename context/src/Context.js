5653430fda57effccf9af0acb4db6fad7b8f29bc
Tej <tejpartapsingh91@gmail.com>
2020-05-03 13:42:43 -0400

    revise

--- context/src/Context.js
+++ context/src/Context.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 const Context = React.createContext();
 
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_ITEM':
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
 export const Consumer = Context.Consumer;
 
 export default ({ children }) => {
-  const [data, setData] = useState({ items: [] });
+  const [data, setData] = useState({
+    items: [],
+    dispatch: (action) => setData((state) => Reducer(state, action)),
+  });
   useEffect(() => {
     (async () => {
       const request = await fetch(`./Data.json`);
--- context/src/components/Item.js
+++ context/src/components/Item.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../Context';
 
 const Item = ({ item }) => {
   const [toggle, setToggle] = useState(false);
   const handleSettings = () => {
     setToggle(!toggle);
   };
-  // const handleDelete = (e, id) => {
-  //   setData({ ...data, items: data.items.filter((item) => item.id !== id) });
-  // };
+  const handleDelete = (e, id, dispatch) =>
+    dispatch({ type: 'DELETE_ITEM', payload: id });
   return (
-    <div>
-      <div>{item.name}</div>
-      <div>{item.age}</div>
-      <button onClick={handleSettings}>Settings</button>
-      {toggle ? <div>These are my settings...</div> : null}
-      {/* <button onClick={(e) => handleDelete(e, item.id)}>Delete</button> */}
-    </div>
+    <Consumer>
+      {(value) => {
+        const { dispatch } = value;
+        return (
+          <div>
+            <div>{item.name}</div>
+            <div>{item.age}</div>
+            <button onClick={handleSettings}>Settings</button>
+            {toggle ? <div>These are my settings...</div> : null}
+            <button onClick={(e) => handleDelete(e, item.id, dispatch)}>
+              Delete
+            </button>
+          </div>
+        );
+      }}
+    </Consumer>
   );
 };
 
