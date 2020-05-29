9d9660664c0a9c44c9d8ede917f66fecd8f8f581
Tej <tejpartapsingh91@gmail.com>
2020-05-29 19:01:56 -0400

    add node sass to gatsby

---
+++ js-experiences/src/components/Layout.js
@@ -0,0 +1,16 @@
+import '../styles/index.scss'
+import React from 'react'
+import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navigation />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
