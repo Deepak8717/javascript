81455496ae882bebf40433518ecb7354aae104ce
Tej <tejpartapsingh91@gmail.com>
2020-05-30 14:15:47 -0400

    add contentful

--- js-experiences/gatsby-config.js
+++ js-experiences/gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -33,6 +37,13 @@ module.exports = {
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
       },
     },
   ],
--- js-experiences/src/pages/blog.js
+++ js-experiences/src/pages/blog.js
@@ -5,33 +5,28 @@ import { graphql, useStaticQuery, Link } from 'gatsby'
 const Blog = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
   `)
-  const { edges } = data.allMarkdownRemark
+  const { edges } = data.allContentfulPost
   return (
     <Layout>
       <ul>
         {edges.map((i, idx) => {
-          const { title, date } = i.node.frontmatter
-          const { slug } = i.node.fields
+          const { title, publishedDate, slug } = i.node
           return (
             <li key={idx}>
               <Link to={`/blog/${slug}`}>
                 <h1>{title}</h1>
-                <p>{date}</p>
+                <p>{publishedDate}</p>
               </Link>
             </li>
           )
