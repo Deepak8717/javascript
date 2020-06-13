0273fb22b48799642693e1ba69c11391a305e2b2
Tej <tejpartapsingh91@gmail.com>
2020-06-13 01:17:47 -0400

    revise

---
+++ 43/script.js
@@ -0,0 +1,54 @@
+const root = document.getElementById('root');
+
+const getRandom = (min, max) => {
+  return Math.ceil(Math.random() * (max - min) + min);
+};
+
+const getData = async () => {
+  const r = await fetch(
+    `https://cors-unlimited.herokuapp.com/https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web`
+  );
+  const t = await r.text();
+  return t;
+};
+
+getData()
+  .then((d) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(d, 'text/html');
+    const list = doc.querySelector('.quick-links > div > ol');
+    const linkSettings = doc.querySelectorAll('a[href^="/en-US/"');
+    const listSettings = doc.querySelectorAll('li');
+    const nodeSettings = doc.querySelectorAll('.toggle');
+    const detailsSettings = doc.querySelectorAll('details');
+    linkSettings.forEach((i) => {
+      const currentSrc = i.getAttribute('href');
+      i.setAttribute('href', `https://developer.mozilla.org${currentSrc}`);
+      i.setAttribute('target', `_blank`);
+    });
+    listSettings.forEach((i) => {
+      i.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(
+        0,
+        255
+      )}, ${getRandom(0, 255)})`;
+      i.setAttribute('data-default-state', 'closed');
+    });
+    nodeSettings.forEach((i) => {
+      i.querySelectorAll('ol').forEach((j) => {
+        j.classList.add('children');
+      });
+      i.querySelectorAll('li').forEach((j) => {
+        j.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(
+          0,
+          255
+        )}, ${getRandom(0, 255)})`;
+      });
+      i.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(
+        0,
+        255
+      )}, ${getRandom(0, 255)})`;
+    });
+    detailsSettings.forEach((i) => i.removeAttribute('open'));
+    root.innerHTML = list.innerHTML;
+  })
+  .catch((e) => console.log(e));
