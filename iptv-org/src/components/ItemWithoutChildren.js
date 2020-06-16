316cc45f46bf0c1d95ac05245f16bb8d20e238ef
Tej <tejpartapsingh91@gmail.com>
2020-06-16 15:24:47 -0400

    revise

--- iptv-org/src/components/ItemWithoutChildren.js
+++ iptv-org/src/components/ItemWithoutChildren.js
@@ -5,8 +5,13 @@ import ClipboardJS from 'clipboard/dist/clipboard.min';
 const ItemWithoutChildren = ({ index, source, title }) => {
   const createMarkup = (source) => ({ __html: source });
   document.querySelectorAll('code').forEach((i) => {
-    const clipboard = new ClipboardJS(i);
-    i.setAttribute('data-clipboard-text', i.textContent);
+    let newNode = document.createElement('button');
+    newNode.classList.add('btn', 'btn-secondary', 'btn-sm');
+    newNode.textContent = 'Copy';
+    i.insertAdjacentElement('beforebegin', newNode);
+    const button = i.parentElement.querySelector('button');
+    const clipboard = new ClipboardJS(button);
+    button.setAttribute('data-clipboard-text', i.textContent);
     clipboard.on('success', (e) => {
       e.trigger.parentElement.parentElement.parentElement
         .querySelectorAll('td')
