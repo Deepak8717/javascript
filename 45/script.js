a929499e7cfd1f6068967abc7d536779d77fb3e5
Tej <tejpartapsingh91@gmail.com>
2020-06-20 15:38:47 -0400

    revise

--- 45/script.js
+++ 45/script.js
@@ -1,46 +1,69 @@
-const generate = document.getElementById('generate');
-const captcha = document.getElementById('captcha');
-const image = document.getElementById('image');
+class Captcha {
+  constructor(font, align, baseline, width, height, bgColor, color) {
+    this.font = font;
+    this.align = align;
+    this.baseline = baseline;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.bgColor = bgColor;
+    this.getImage = () => {
+      const captcha = document.createElement('canvas');
+      const newImage = document.createElement('image');
+      newImage.id = 'canvas';
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+      const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+      };
 
-const getCharacter = () => {
-  const arr = [];
-  arr.push(getRandomInt(97, 122));
-  arr.push(getRandomInt(65, 90));
-  arr.push(getRandomInt(48, 57));
-  const value = arr[getRandomInt(0, arr.length)];
-  return String.fromCharCode(value);
-};
+      const getCharacter = () => {
+        const arr = [];
+        arr.push(getRandomInt(97, 122));
+        arr.push(getRandomInt(65, 90));
+        arr.push(getRandomInt(48, 57));
+        const value = arr[getRandomInt(0, arr.length)];
+        return String.fromCharCode(value);
+      };
 
-const makeImage = () => {
-  const newWord = getWord();
-  const context = captcha.getContext('2d');
-  context.canvas.width = 300;
-  context.canvas.height = 150;
-  context.font = '48px Courier New';
-  context.textAlign = 'center';
-  context.textBaseline = 'middle';
-  context.fillStyle = '#eee';
-  context.fillRect(0, 0, 300, 150);
-  context.fillStyle = '#222';
-  context.fillText(newWord, 150, 75);
-  image.src = context.canvas.toDataURL();
-  return false;
-};
+      const getWord = () => {
+        let newWord = ``;
+        let count = 0;
+        while (count < 6) {
+          newWord += getCharacter();
+          count++;
+        }
+        return newWord;
+      };
 
-const getWord = () => {
-  let newWord = ``;
-  let count = 0;
-  while (count < 6) {
-    newWord += getCharacter();
-    count++;
+      const newWord = getWord();
+      const context = captcha.getContext('2d');
+      context.canvas.width = this.width;
+      context.canvas.height = this.height;
+      context.font = this.font;
+      context.textAlign = this.align;
+      context.textBaseline = this.baseline;
+      context.fillStyle = this.bgColor;
+      context.fillRect(0, 0, this.width, this.height);
+      context.fillStyle = this.color;
+      context.fillText(newWord, this.width / 2, this.height / 2);
+      newImage.setAttribute('src', context.canvas.toDataURL());
+      return newImage;
+    };
   }
-  return newWord;
-};
+}
 
-generate.addEventListener('click', () => makeImage());
+const MyCaptcha = new Captcha(
+  '48px Courier New',
+  'center',
+  'middle',
+  300,
+  150,
+  '#eee',
+  '#222'
+);
+
+document.getElementById('generate').addEventListener('click', () => {
+  document.getElementById('banner').innerHTML = MyCaptcha.getImage().outerHTML;
+});
