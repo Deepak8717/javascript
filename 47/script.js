1bcb94a3b69a8823ac453e355f3f6b3f0306de82
Tej <tejpartapsingh91@gmail.com>
2020-07-31 15:01:15 -0400

    revise

--- 47/script.js
+++ 47/script.js
@@ -1,3 +1,5 @@
+let currentLanguage = '';
+
 const executeLoop = (selection, largeSet) => {
   const letterSet = [...largeSet];
   let minLength = 0;
@@ -52,6 +54,30 @@ const printAlphabets = () => {
   printLetters(smallSet, largeSet);
 };
 
+const changeLanguage = (selectedLanguage) => {
+  switch (selectedLanguage) {
+    case 'punjabi':
+      currentLanguage = selectedLanguage;
+      printLetters(['ਕ', 'ਹ', 'ਲ'], ['ਬ', 'ਮ', 'ਫ', 'ਧ', 'ਛ', 'ਗ', 'ਡ', 'ਥ', 'ਢ', 'ਐ']);
+      break;
+    case 'hindi':
+      currentLanguage = selectedLanguage;
+      printLetters(['घ', 'ख', 'ग'], ['ब', 'फ', 'प', 'ध', 'ज', 'ड', 'ट', 'थ', 'ण', 'भ']);
+      break;
+    case 'vietnamese':
+      currentLanguage = selectedLanguage;
+      printLetters(['Č', 'Š', 'Ê'], ['Ư', 'Ê', 'Ă', 'Ž', 'Š', 'Đ', 'Ơ', 'Â', 'Ć', 'Č']);
+      break;
+    default:
+      currentLanguage = '';
+      printLetters(
+        ['E', 'H', 'N'],
+        ['C', 'D', 'H', 'K', 'N', 'O', 'R', 'S', 'V', 'Z']
+      );
+      break;
+  }
+};
+
 printAlphabets();
 
 // Mode
@@ -60,15 +86,40 @@ mode.addEventListener('click', () => {
   document.body.classList.toggle('mode');
 });
 
+// Language
+const language = document.getElementById('language');
+language.addEventListener('click', () => {
+  document.body.classList.toggle('language');
+});
+
+// Language Form
+const languageForm = document.querySelector('.language-form');
+languageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const selectedLanguage = e.target.elements['language-list'].value;
+  document.body.classList.remove('language');
+  changeLanguage(selectedLanguage);
+  reset.style.display = 'block';
+});
+
 // Reset
 const reset = document.getElementById('reset');
+reset.style.display = 'none';
 reset.addEventListener('click', () => {
+  changeLanguage(currentLanguage);
+});
+
+// English
+const english = document.getElementById('english');
+english.addEventListener('click', () => {
+  reset.style.display = 'none';
   printAlphabets();
 });
 
 // Numbers
 const numbers = document.getElementById('numbers');
 numbers.addEventListener('click', () => {
+  reset.style.display = 'none';
   const smallSet = [0, 1, 2, 3];
   const largeSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   printLetters(smallSet, largeSet);
