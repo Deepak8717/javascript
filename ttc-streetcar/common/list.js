83d42f95880fc3007ff0508823401e99eca3b6ec
Tej <tkahlon@labx.com>
2021-06-19 15:26:49 -0400

    add streetcar

---
+++ ttc-streetcar/common/list.js
@@ -0,0 +1,104 @@
+const list = [
+  {
+    line: 501,
+    direction: "EB",
+    video: "https://youtu.be/Deogvppc0K4?t=63",
+  },
+  {
+    line: 501,
+    direction: "WB",
+    video: "https://youtu.be/nGFYo5A7EK8",
+  },
+  {
+    line: 503,
+    direction: "EB",
+    video: "https://youtu.be/hGj7TQUAQjQ?t=33",
+  },
+  {
+    line: 503,
+    direction: "WB",
+    video: "https://youtu.be/1puG7NPjlIk?t=17",
+  },
+  {
+    line: 504,
+    direction: "EB",
+    video: "https://youtu.be/y4vc3msbtP0",
+  },
+  {
+    line: 504,
+    direction: "WB",
+    video: "https://youtu.be/bELG0gB4h5E",
+  },
+  {
+    line: 505,
+    direction: "EB",
+    video: "https://youtu.be/YyFLQ8LBiAw?t=33",
+  },
+  {
+    line: 505,
+    direction: "WB",
+    video: "https://youtu.be/TGV8llDmjl8",
+  },
+  {
+    line: 506,
+    direction: "EB",
+    video: "https://youtu.be/xY4k0kzujf8?t=120",
+  },
+  {
+    line: 506,
+    direction: "WB",
+    video: "https://youtu.be/avKMbCLb3x4?t=33",
+  },
+  {
+    line: 508,
+    direction: "EB",
+    video: "https://youtu.be/HF5B0haQs_c?t=101",
+  },
+  // {
+  //   line: 508,
+  //   direction: "WB",
+  //   video: "https://youtu.be/test?t=00",
+  // },
+  {
+    line: 509,
+    direction: "EB",
+    video: "https://youtu.be/Zj7sLBuWMUc?t=35",
+  },
+  {
+    line: 509,
+    direction: "WB",
+    video: "https://youtu.be/w3jV6kDt6ls?t=09",
+  },
+  {
+    line: 510,
+    direction: "NB",
+    video: "https://youtu.be/NhCpWymVKIo?t=96",
+  },
+  {
+    line: 510,
+    direction: "SB",
+    video: "https://youtu.be/iNUdeL1_Q_Q?t=35",
+  },
+  {
+    line: 511,
+    direction: "NB",
+    video: "https://youtu.be/mZy7uJA7mKk?t=40",
+  },
+  {
+    line: 511,
+    direction: "SB",
+    video: "https://youtu.be/ltCkM932v6g?t=27",
+  },
+  {
+    line: 512,
+    direction: "EB",
+    video: "https://youtu.be/RojzehF3_38?t=33",
+  },
+  {
+    line: 512,
+    direction: "WB",
+    video: "https://youtu.be/enaD-33GBvU?t=13",
+  },
+];
+
+export default list;
---
+++ ttc-streetcar/pages/index.js
@@ -0,0 +1,86 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import list from "../common/list";
+
+export default function Home() {
+  const [data, setData] = useState({
+    station: 0,
+    direction: null,
+  });
+  const { station, direction } = data;
+  const handleClick = (station, direction) => {
+    setData({
+      station,
+      direction,
+    });
+  };
+  return (
+    <>
+      <Head>
+        <title>TTC Streetcar</title>
+        <link
+          rel="icon"
+          href="https://upload.wikimedia.org/wikipedia/commons/8/87/TTC.svg"
+        />
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+      </Head>
+
+      {station === 0 && direction === null ? (
+        <main>
+          <aside>
+            <a
+              className="github-button"
+              href="https://github.com/tpkahlon/javascript"
+              data-color-scheme="no-preference: light; light: light; dark: dark;"
+              data-show-count="true"
+              aria-label="Star tpkahlon/javascript on GitHub"
+            >
+              Star
+            </a>
+          </aside>
+          <a
+            className="promotion"
+            href="https://ttc-subway.vercel.app"
+            target="__blank"
+            rel="noopener noreferrer"
+          >
+            Check out TTC Subway
+          </a>
+          <h1>Please pick your streetcar of choice:</h1>
+          <p>
+            This application allow rider/user to take a virtual ride in street
+            cars. This application may be useful for accessibility purposes. It
+            is recommended to buffer/sync entire video on your device before
+            starting the trip. Source code for this application is located in{" "}
+            <code>ttc-streetcar</code> folder.
+          </p>
+          <div className="lines">
+            {list.map(({ line, direction, video }, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="line"
+                  onClick={() => handleClick(line, video)}
+                >
+                  {line}
+                  <span>{direction}</span>
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      ) : (
+        <main>
+          <ReactPlayer
+            url={`./${station}-${direction}.mp4`}
+            controls
+            playing
+            width="100vw"
+            height="100vh"
+          />
+        </main>
+      )}
+    </>
+  );
+}
